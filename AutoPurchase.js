ab05c12dadce05a415392708cc8d4e7633824062
frionx <56565111+frionx@users.noreply.github.com>
2020-11-14 19:26:10 -0600

    Create AutoPurchase.js

---
+++ AutoPurchase.js
@@ -0,0 +1,17 @@
+RBLXApi.loopCount = 0;
+RBLXApi.MaxLoop = 10;
+RBLXApi.RunScript = function(){
+  if (RBLXApi.robuxCount >= 10 && RBLXApi.robuxCount <= 100){
+    RBLXApi.BuyGamepass("id", true); // true to automatically delete gamepass after purchase to be bought more time, use just RBLXApi.BuyGamepass("id"); to not buy it multiple times
+  }
+  else if (RBLXApi.robuxCount >= 100 && RBLXApi.robuxCount <= 500){
+    RBLXApi.BuyGamepass("id", true); // Buy gamepass selling for 100 robux
+  }
+  else if (RBLXApi.robuxCount >= 500){
+    RBLXApi.BuyGamepass("id", true); // Buy the gamepass selling for 500 robux
+  }
+  RBLXApi.loopCount += 1;
+  if (RBLXApi.MaxLoop >= loopCount){
+    RBLXApi.launchScript(RBLXApi.userId); // Recounts robux and will relaunch this script, since its ASYNC, you can't just do a get request and save it to a variable, thus you call this function again
+  }
+}
